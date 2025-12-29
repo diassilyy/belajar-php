0923e0398e37369252c268873216eb0330ab88d8
diassilyy <muhamaddiasilyasa94@gmail.com>
2025-12-29 15:01:31 +0700

    learning variabel assigment

---
+++ variabel/comments/data-types/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** 
+ * string
+ * can includes any characters
+*/
+
+$string_a = 'Dias \'12345678910';
+$string_b = "Ilyasa !^@&@";
+
+// concatenation
+
+$concatenation = $string_a . ' ' . $string_b;
+// echo $concatenation
---
+++ variabel/comments/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// var_dump($a + $b);
+
+$a = 10;
+$b = 20;
+
+$c = $a * $b;
+
+var_dump($c);
+
+// <-- comment
+
+/*
+   multi-line comment
+
+*/
+
+/**
+ * multi-line comment
+ */
